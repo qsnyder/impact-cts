cf258dfb0483e8bba2e078dfb39a17a68bcbde88
quinn snyder <snyderq@gmail.com>
2022-07-14 20:09:33 -0700

    Fixing the setup script

--- setup.sh
+++ setup.sh
@@ -8,25 +8,25 @@ git clone https://github.com/qsnyder/impact-cts.git
 echo "##################################################################"
 echo "# Downloading Consul-Terraform-Sync (CTS) binary.... #############"
 echo "##################################################################"
-curl https://releases.hashicorp.com/consul-terraform-sync/0.6.0/consul-terraform-sync_0.6.0_linux_amd64.zip -o cts-esg-sample-code/cts/cts.zip
-unzip cts-esg-sample-code/cts/cts.zip -d cts-esg-sample-code/cts/
-rm -rf cts-esg-sample-code/cts/cts.zip
+curl https://releases.hashicorp.com/consul-terraform-sync/0.6.0/consul-terraform-sync_0.6.0_linux_amd64.zip -o impact-cts/cts/cts.zip
+unzip impact-cts/cts/cts.zip -d impact-cts/cts/
+rm -rf impact-cts/cts/cts.zip
 
 echo "##################################################################"
 echo "# Downloading Terraform binary.... ###############################"
 echo "##################################################################"
-curl https://releases.hashicorp.com/terraform/1.1.9/terraform_1.1.9_linux_amd64.zip -o cts-esg-sample-code/pre-req/tf.zip
-unzip cts-esg-sample-code/pre-req/tf.zip -d cts-esg-sample-code/pre-req/
-rm -rf cts-esg-sample-code/pre-req/tf.zip
+curl https://releases.hashicorp.com/terraform/1.1.9/terraform_1.1.9_linux_amd64.zip -o impact-cts/pre-req/tf.zip
+unzip impact-cts/pre-req/tf.zip -d impact-cts/pre-req/
+rm -rf impact-cts/pre-req/tf.zip
 
 echo "##################################################################"
 echo "# Ensuring scripts are executable.... ############################"
 echo "##################################################################"
-chmod +x cts-esg-sample-code/cts/app-exec.sh
-chmod +x cts-esg-sample-code/cts/app01-start.sh
-chmod +x cts-esg-sample-code/cts/app02-start.sh
-chmod +x cts-esg-sample-code/cts/consul-srv-start.sh
-chmod +x cts-esg-sample-code/cts/cts-start.sh
+chmod +x impact-cts/cts/app-exec.sh
+chmod +x impact-cts/cts/app01-start.sh
+chmod +x impact-cts/cts/app02-start.sh
+chmod +x impact-cts/cts/consul-srv-start.sh
+chmod +x impact-cts/cts/cts-start.sh
 
 echo "##################################################################"
 echo "# Pulling Consul server container.... ############################"
@@ -36,7 +36,7 @@ docker pull consul
 echo "##################################################################"
 echo "# Building application container.... #############################"
 echo "##################################################################"
-docker build -t cts-nginx:0.1 cts-esg-sample-code/docker/.
+docker build -t cts-nginx:0.1 impact-cts/docker/.
 
 echo ""
 echo "##################################################################"
